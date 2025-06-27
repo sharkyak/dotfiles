3c852bbed4b3eec467490a09db063222993a3859
Aleksandr Kazakov <aleksandr.kazakov@bambinifashion.com>
2025-06-27 14:07:17 +0700

    add syntax highlight

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -16,13 +16,14 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "html", "css", "yaml", "dockerfile",
+        "python"
+  		},
+  	},
+  },
 }
